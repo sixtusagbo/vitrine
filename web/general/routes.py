d0ad94e81818de15612e0496426875fcc17f82a1
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-07-15 12:59:55 +0100

    Update brand page

--- web/general/routes.py
+++ web/general/routes.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
 """Routes for the general blueprint"""
 from web.general import general_bp
-from flask import render_template, abort
+from flask import render_template
 import uuid
-from models import storage
 
 
 @general_bp.route("/<handle>")
 def brand(handle):
     """Return a brand's page"""
-    brand = storage.get_brand(handle)
-    if not brand:
-        abort(404)
-    meta = {}
-
-    meta["name"] = brand.name
-    meta["handle"] = brand.handle
-    meta["description"] = brand.description
-    meta["cover_image"] = brand.cover_image
-    meta["address"] = brand.address
-
-    return render_template("brand.html", meta=meta, cache_id=uuid.uuid4(),
-                           is_solopreneur=brand.is_solopreneur)
+    return render_template("brand.html", cache_id=uuid.uuid4())
--- web/static/scripts/brand.js
+++ web/static/scripts/brand.js
@@ -1,18 +1,51 @@
 // for dynamic url
-const {protocol, hostname} = window.location;
+const { protocol, hostname, pathname, href: currentUrl } = window.location;
 const apiUrl = `${protocol}//${hostname}:5001/api/v1`;
 let brand = {};
-const pathname = window.location.pathname;
-const handle = pathname[0] === '/' ? pathname.substr(1) : pathname;
+const handle = pathname.substr(1);
 
 const populateDOM = () => {
+  // inside head tag
+  $('title').text(`${brand.name} | Vitrine`);
+  $('meta[name="title"]').attr('content', `${brand.name} | Vitrine`);
+  $('meta[property="og:title"]').attr('content', `${brand.name} | Vitrine`);
+  $('meta[property="twitter:title"]').attr('content', `${brand.name} | Vitrine`);
+  $('meta[property="og:url"]').attr('content', currentUrl);
+  $('meta[property="twitter:url"]').attr('content', currentUrl);
+
+  if (brand.description) {
+    $('meta[name="description"]').attr('content', brand.description);
+    $('meta[property="og:description"]').attr('content', brand.description);
+    $('meta[property="twitter:description"]').attr('content', brand.description);
+  } else {
+    $('meta[name="description"]').remove();
+    $('meta[property="og:description"]').remove();
+    $('meta[property="twitter:description"]').remove();
+  }
+
+  if (brand.cover_image) {
+    $('meta[property="og:image"]').attr('content', brand.cover_image);
+    $('meta[property="twitter:image"]').attr('content', brand.cover_image);
+  } else {
+    $('meta[property="og:image"]').remove();
+    $('meta[property="twitter:image"]').remove();
+  }
+
+  // inside body tag
+  $('.brandname').text(brand.name);
   if (brand.statement) $('.header #statement').text(brand.statement);
   if (brand.description) $('.description').text(brand.description);
   if (brand.cover_image) $('.header #cover_image').attr('src', brand.cover_image);
   if (brand.detail_lead) $('#details #detail_lead').text(brand.detail_lead);
   if (brand.detail_image) $('#details #detail_image').attr('src', brand.detail_image);
+
+  if (brand.is_solopreneur) {
+    $('.solopreneur').text(brand.is_solopreneur ? 'me' : 'us');
+    $('footer .solopreneur').text(brand.is_solopreneur ? 'I' : 'We');
+  }
+
   if (brand.detail_points) {
-    $('#detail_points').html('');
+    $('#detail_points').empty();
 
     for (const point of brand.detail_points) {
       let listItem = '<li class="d-flex">';
@@ -26,6 +59,10 @@ const populateDOM = () => {
   }
 
   populateWorks();
+
+  if (brand.address) {
+    $('#googleMaps').attr('src', `https://www.google.com/maps/embed/v1/place?key=AIzaSyCNveGQ9bfpKFwWzQLLftrR9hNiHwdqQG8&q=${brand.address}`);
+  }
 
   if (brand.twitter_url) {
     $('#twitter-link').attr('href', brand.twitter_url);
@@ -55,7 +92,7 @@ const populateDOM = () => {
   $('#brand-email strong').text(brand.email);
 
   $('#whatsapp-btn').attr('href', `https://wa.me/${brand.whatsapp_no}`);
-  
+
   const createdAt = new Date(brand.created_at);
   $('#created-year').text(createdAt.getFullYear());
 };
@@ -64,12 +101,11 @@ const populateWorks = async () => {
   await $.get(`${apiUrl}/brands/${handle}/works`,
     function (data) {
       brand.works = data;
-      console.log(brand);
     }
   );
 
-  if (brand.works) {
-    $('#works').html('');
+  if (brand.works.length) {
+    $('#works').empty();
 
     for (const work of brand.works) {
       let project = '<div class="card">';
@@ -85,11 +121,10 @@ const populateWorks = async () => {
 };
 
 const setup = () => {
-  $.get(`${apiUrl}/brands/${handle}`,
+  $.get(`${apiUrl}/brands/${handle}/public`,
     function (data) {
       brand = data;
 
-      $('.brandname').text(brand.name);
       populateDOM();
     }
   );
